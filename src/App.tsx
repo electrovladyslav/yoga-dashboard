7cf4585b67739df781b4febda1b12e113d717d4c
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2024-08-25 13:29:31 +0300

    Init commit.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import cardImage from '/card-stub-image.jpeg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Yoga dashboard</h1>
+      <img src={cardImage} className="logo" alt="Vite logo" />
+    </>
+  )
+}
+
+export default App
